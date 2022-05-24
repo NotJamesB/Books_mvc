d72291ed49e578fe0b4123eeea5f886d4d4b0e4e
NotJamesB <97855508+NotJamesB@users.noreply.github.com>
2022-05-23 17:22:52 -0700

    Add files via upload

---
+++ src/main/java/com/codingdojo/books/MvccApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvccApplication.class, args);
+	}
+
+}
